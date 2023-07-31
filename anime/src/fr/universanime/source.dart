5089af6014f244d39d61178854481d6d229106b4
kodjomoustapha <107993382+kodjodevf@users.noreply.github.com>
2023-07-31 22:20:58 +0100

    fix universanime source url

--- anime/src/fr/universanime/source.dart
+++ anime/src/fr/universanime/source.dart
@@ -1,7 +1,7 @@
 import '../../../../model/source.dart';
 
 Source get universanimeSource => _universanimeSource;
-const universanimeVersion = "0.0.11";
+const universanimeVersion = "0.0.12";
 const universanimeSourceCodeUrl =
     "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/main/anime/src/fr/universanime/universanime-v$universanimeVersion.dart";
 Source _universanimeSource = Source(
