2e33247878ab7e59443001d1f795f60aa7e3795a
kodjomoustapha <107993382+kodjodevf@users.noreply.github.com>
2023-08-02 23:45:18 +0100

    update anime_index

--- anime/src/en/wcostream/source.dart
+++ anime/src/en/wcostream/source.dart
@@ -1,9 +1,9 @@
 import '../../../../model/source.dart';
 
 Source get wcostreamSource => _wcostreamSource;
-const wcostreamVersion = "0.0.11";
+const wcostreamVersion = "0.0.1";
 const wcostreamSourceCodeUrl =
-    "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/main/anime/src/fr/wcostream/wcostream-v$wcostreamVersion.dart";
+    "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/main/anime/src/en/wcostream/wcostream-v$wcostreamVersion.dart";
 Source _wcostreamSource = Source(
     name: "WCOStream",
     baseUrl: "https://www.wcostream.org",
