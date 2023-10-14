3d28d38c298af785a0cbb55962b01a1f035842b9
kodjomoustapha <107993382+kodjodevf@users.noreply.github.com>
2023-10-14 21:57:20 +0100

    New source : Okanime (AR)

---
+++ anime/src/ar/source.dart
@@ -0,0 +1,17 @@
+import '../../../model/source.dart';
+import '../../../utils/utils.dart';
+
+Source get okanimeSource => _okanimeSource;
+const okanimeVersion = "0.0.1";
+const okanimeSourceCodeUrl =
+    "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/main/anime/src/ar/okanime/okanime-v$okanimeVersion.dart";
+Source _okanimeSource = Source(
+    name: "Okanime",
+    baseUrl: "https://www.okanime.xyz",
+    lang: "ar",
+    typeSource: "single",
+    iconUrl: getIconUrl("okanime", "ar"),
+    sourceCodeUrl: okanimeSourceCodeUrl,
+    version: okanimeVersion,
+    appMinVerReq: "0.0.48",
+    isManga: false);
