a6bd97d4c6f9a7c119548fc1efbaf7976515fcc8
kodjomoustapha <107993382+kodjodevf@users.noreply.github.com>
2023-12-08 16:35:29 +0100

    Update

--- manga/multisrc/mangareader/src/komiklab/komiklab.dart
+++ manga/multisrc/mangareader/src/komiklab/komiklab.dart
@@ -1,14 +1,15 @@
 import '../../../../../model/source.dart';
 
-  Source get komiklabSource => _komiklabSource;
-            
-  Source _komiklabSource = Source(
+Source get komiklabSource => _komiklabSource;
+
+Source _komiklabSource = Source(
+    id: 932554594,
     name: "KomikLab Scans",
     baseUrl: "https://komiklab.com",
     lang: "en",
-    isNsfw:true,
+    isNsfw: true,
     typeSource: "mangareader",
-    iconUrl:"https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/main/manga/multisrc/mangareader/src/komiklab/icon.png",
-    dateFormat:"MMMM dd, yyyy",
-    dateFormatLocale:"en_us",
-  );
+    iconUrl:
+        "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/main/manga/multisrc/mangareader/src/komiklab/icon.png",
+    dateFormat: "MMMM dd, yyyy",
+    dateFormatLocale: "en_us");
