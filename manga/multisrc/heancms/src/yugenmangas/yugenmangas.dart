49d9118b8cab49baceb0bcf7599a46aa400e9cb9
kodjomoustapha <107993382+kodjodevf@users.noreply.github.com>
2023-12-08 16:24:50 +0100

    +

--- manga/multisrc/heancms/src/yugenmangas/yugenmangas.dart
+++ manga/multisrc/heancms/src/yugenmangas/yugenmangas.dart
@@ -1,14 +1,15 @@
 import '../../../../../model/source.dart';
 
-  Source get yugenmangasSource => _yugenmangasSource;
-            
-  Source _yugenmangasSource = Source(
-    name: "YugenMangas",
-    baseUrl: "https://yugenmangas.lat",
-    lang: "es",
-    
-    typeSource: "heancms",
-    iconUrl:"https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/main/manga/multisrc/heancms/src/yugenmangas/icon.png",
-    dateFormat:"yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ",
-    dateFormatLocale:"en",
-  );
+Source get yugenmangasSource => _yugenmangasSource;
+
+Source _yugenmangasSource = Source(
+  name: "YugenMangas",
+  baseUrl: "https://yugenmangas.lat",
+  apiUrl: "https://api.yugenmangas.net",
+  lang: "es",
+  typeSource: "heancms",
+  iconUrl:
+      "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/main/manga/multisrc/heancms/src/yugenmangas/icon.png",
+  dateFormat: "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ",
+  dateFormatLocale: "en",
+);
