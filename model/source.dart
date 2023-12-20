808529284c6b9fce8d7ae243ebd0bfade1834dfe
kodjomoustapha <107993382+kodjodevf@users.noreply.github.com>
2023-12-20 18:39:03 +0100

    Update

--- model/source.dart
+++ model/source.dart
@@ -49,7 +49,7 @@ class Source {
       this.version = "",
       this.isManga = true,
       this.isFullData = false,
-      this.appMinVerReq = "0.1.5",
+      this.appMinVerReq = "0.1.45",
       this.additionalParams = ""});
 
   Map<String, dynamic> toJson() {
