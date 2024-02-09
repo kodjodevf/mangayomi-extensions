8bad1e2eeebcdfefa4b30f1ef2cb3612a22ad25e
kodjomoustapha <107993382+kodjodevf@users.noreply.github.com>
2024-02-09 14:33:49 +0100

    fix

--- anime/src/en/animepahe/source.dart
+++ anime/src/en/animepahe/source.dart
@@ -1,16 +1,16 @@
 import '../../../../model/source.dart';
 
 Source get animepaheSource => _animepaheSource;
-const _animepaheVersion = "0.0.1";
+const _animepaheVersion = "0.0.15";
 const _animepaheSourceCodeUrl =
-    "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/$branchName/anime/src/ar/animepahe/animepahe.dart";
+    "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/$branchName/anime/src/en/animepahe/animepahe.dart";
 Source _animepaheSource = Source(
-    name: "animepahe",
+    name: "AnimePahe",
     baseUrl: "https://www.animepahe.ru",
     lang: "en",
     typeSource: "single",
     iconUrl:
-        "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/$branchName/anime/src/ar/animepahe/icon.png",
+        "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/$branchName/anime/src/en/animepahe/icon.png",
     sourceCodeUrl: _animepaheSourceCodeUrl,
     version: _animepaheVersion,
     isManga: false);
