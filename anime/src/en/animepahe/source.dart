24b4aa68bbd64a49886625df73db9504e7893fe8
kodjomoustapha <107993382+kodjodevf@users.noreply.github.com>
2024-02-09 16:13:23 +0100

    Update

--- anime/src/en/animepahe/source.dart
+++ anime/src/en/animepahe/source.dart
@@ -1,7 +1,7 @@
 import '../../../../model/source.dart';
 
 Source get animepaheSource => _animepaheSource;
-const _animepaheVersion = "0.0.15";
+const _animepaheVersion = "0.0.2";
 const _animepaheSourceCodeUrl =
     "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/$branchName/anime/src/en/animepahe/animepahe.dart";
 Source _animepaheSource = Source(
