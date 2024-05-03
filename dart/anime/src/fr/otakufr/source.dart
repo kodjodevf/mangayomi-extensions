67bb9c88ed752bcbd2627dd2aabfbc637e992567
kodjomoustapha <107993382+kodjodevf@users.noreply.github.com>
2024-05-03 11:44:11 +0100

    OtakuFr fix player

--- dart/anime/src/fr/otakufr/source.dart
+++ dart/anime/src/fr/otakufr/source.dart
@@ -1,7 +1,7 @@
 import '../../../../../model/source.dart';
 
 Source get otakufr => _otakufr;
-const otakufrVersion = "0.0.85";
+const otakufrVersion = "0.0.9";
 const otakufrCodeUrl =
     "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/$branchName/dart/anime/src/fr/otakufr/otakufr.dart";
 Source _otakufr = Source(
