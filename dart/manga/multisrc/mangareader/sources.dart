96b1b5efdf8dbd7569567d3014295cd594e926a9
kodjomoustapha <107993382+kodjodevf@users.noreply.github.com>
2024-06-18 20:12:05 +0100

    Refactor mangareader

--- dart/manga/multisrc/mangareader/sources.dart
+++ dart/manga/multisrc/mangareader/sources.dart
@@ -127,7 +127,7 @@ import 'src/shijiescans/shijiescans.dart';
 import 'src/summertoon/summertoon.dart';
 import 'src/zenithscans/zenithscans.dart';
 
-const mangareaderVersion = "0.1.1";
+const mangareaderVersion = "0.1.15";
 const mangareaderSourceCodeUrl =
     "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/$branchName/dart/manga/multisrc/mangareader/mangareader.dart";
 
