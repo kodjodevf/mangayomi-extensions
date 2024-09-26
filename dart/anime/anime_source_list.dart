6e08887d1ff64f9b5d53063dbe9f98f028ccd523
kodjomoustapha <107993382+kodjodevf@users.noreply.github.com>
2024-09-26 09:25:52 +0100

    -

--- dart/anime/anime_source_list.dart
+++ dart/anime/anime_source_list.dart
@@ -11,7 +11,7 @@ import 'src/en/dramacool/source.dart';
 import 'src/en/gogoanime/source.dart';
 import 'src/en/nineanimetv/source.dart';
 import 'src/fr/animesama/source.dart';
-import 'src/fr/anizone_/source.dart';
+import 'src/fr/anizone/source.dart';
 import 'src/hi/yomovies/source.dart';
 import 'src/en/kisskh/source.dart';
 import 'src/en/uhdmovies/source.dart';
