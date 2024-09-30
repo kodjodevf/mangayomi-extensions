43fcb00405b0c272b483f5f5febfaf3b888f2ced
kodjomoustapha <107993382+kodjodevf@users.noreply.github.com>
2024-09-30 16:33:13 +0100

    New source:  AnimeOnlineNinja (es)

---
+++ dart/anime/src/es/animeonlineninja/source.dart
@@ -0,0 +1,16 @@
+import '../../../../../model/source.dart';
+
+Source get animeonlineninjaSource => _animeonlineninjaSource;
+const _animeonlineninjaVersion = "0.0.1";
+const _animeonlineninjaSourceCodeUrl =
+    "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/$branchName/dart/anime/src/es/animeonlineninja/animeonlineninja.dart";
+Source _animeonlineninjaSource = Source(
+    name: "AnimeOnline.Ninja",
+    baseUrl: "https://ww3.animeonline.ninja",
+    lang: "es",
+    typeSource: "single",
+    iconUrl:
+        "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/$branchName/dart/anime/src/es/animeonlineninja/icon.png",
+    sourceCodeUrl: _animeonlineninjaSourceCodeUrl,
+    version: _animeonlineninjaVersion,
+    isManga: false);
