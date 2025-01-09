3c667e59f5e9e1ce652a413af51ff3221ab1703a
kodjomoustapha <107993382+kodjodevf@users.noreply.github.com>
2025-01-09 14:40:22 +0100

    fix

--- dart/manga/manga_source_list.dart
+++ dart/manga/manga_source_list.dart
@@ -4,13 +4,11 @@ import 'multisrc/mangabox/sources.dart';
 import 'multisrc/mangareader/sources.dart';
 import 'multisrc/mmrcms/sources.dart';
 import 'multisrc/nepnep/sources.dart';
-import 'src/all/comick/sources.dart';
 import 'src/all/mangadex/sources.dart';
 import 'src/en/mangahere/source.dart';
 
 List<Source> dartMangasourceList = [
   ...madaraSourcesList,
-  ...comickSourcesList,
   ...mangaDexSourcesList,
   ...mangareaderSourcesList,
   ...mmrcmsSourcesList,
