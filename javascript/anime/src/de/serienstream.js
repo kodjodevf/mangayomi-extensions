d5113bedc5b45ae2ddf14a17a85c4d495cc736c7
Moustapha Kodjo Amadou <kodjomoustapha@gmail.com>
2025-01-12 23:17:04 +0100

    Update serienstream.js

--- javascript/anime/src/de/serienstream.js
+++ javascript/anime/src/de/serienstream.js
@@ -7,7 +7,7 @@ const mangayomiSources = [{
     "typeSource": "single",
     "itemType": 1,
     "isNsfw": false,
-    "version": "0.0.4",
+    "version": "0.0.5",
     "dateFormat": "",
     "dateFormatLocale": "",
     "pkgPath": "anime/src/de/serienstream.js"
