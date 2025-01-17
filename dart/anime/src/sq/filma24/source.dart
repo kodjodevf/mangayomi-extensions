ffb977325d7182e6ad4b453fcc8b815e62cfce13
Moustapha Kodjo Amadou <107993382+kodjodevf@users.noreply.github.com>
2025-01-17 11:38:03 +0100

    update filma24

--- dart/anime/src/sq/filma24/source.dart
+++ dart/anime/src/sq/filma24/source.dart
@@ -1,7 +1,7 @@
 import '../../../../../model/source.dart';
 
 Source get filma24 => _filma24;
-const _filma24Version = "0.0.4";
+const _filma24Version = "0.0.45";
 const _filma24CodeUrl =
     "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/$branchName/dart/anime/src/sq/filma24/filma24.dart";
 Source _filma24 = Source(
