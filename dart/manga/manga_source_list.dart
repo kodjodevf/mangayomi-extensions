42158c7d8e314345270e3a77584779f77f39dfca
Moustapha Kodjo Amadou <107993382+kodjodevf@users.noreply.github.com>
2025-01-23 14:59:30 +0100

    rewrite MangaDex source code in JS

--- dart/manga/manga_source_list.dart
+++ dart/manga/manga_source_list.dart
@@ -4,12 +4,10 @@ import 'multisrc/mangabox/sources.dart';
 import 'multisrc/mangareader/sources.dart';
 import 'multisrc/mmrcms/sources.dart';
 import 'multisrc/nepnep/sources.dart';
-import 'src/all/mangadex/sources.dart';
 import 'src/en/mangahere/source.dart';
 
 List<Source> dartMangasourceList = [
   ...madaraSourcesList,
-  ...mangaDexSourcesList,
   ...mangareaderSourcesList,
   ...mmrcmsSourcesList,
   mangahereSource,
