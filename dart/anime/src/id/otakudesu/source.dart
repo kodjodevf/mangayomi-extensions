e6fc5b6a1a7181b78945d60ad64f303218cdac88
Fallah Andy Prakasa <me@flla.my.id>
2025-02-19 22:57:08 +0700

    chore(anime): bump otakudesu version to `v0.0.56`

--- dart/anime/src/id/otakudesu/source.dart
+++ dart/anime/src/id/otakudesu/source.dart
@@ -1,7 +1,7 @@
 import '../../../../../model/source.dart';
 
 Source get otakudesu => _otakudesu;
-const _otakudesuVersion = "0.0.55";
+const _otakudesuVersion = "0.0.56";
 const _otakudesuCodeUrl =
     "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/$branchName/dart/anime/src/id/otakudesu/otakudesu.dart";
 Source _otakudesu = Source(
