7966d45be79257ff0ead92eb79bd436f08968b24
Moustapha Kodjo Amadou <107993382+kodjodevf@users.noreply.github.com>
2025-04-02 17:07:33 +0100

    +

---
+++ dart/anime/src/en/kisskh/source.dart
@@ -0,0 +1,17 @@
+import '../../../../../model/source.dart';
+
+Source get kisskhSource => _kisskhSource;
+const _kisskhVersion = "0.0.65";
+const _kisskhSourceCodeUrl =
+    "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/$branchName/dart/anime/src/en/kisskh/kisskh.dart";
+Source _kisskhSource = Source(
+  name: "KissKH",
+  baseUrl: "https://kisskh.co",
+  lang: "en",
+  typeSource: "single",
+  iconUrl:
+      "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/$branchName/dart/anime/src/en/kisskh/icon.png",
+  sourceCodeUrl: _kisskhSourceCodeUrl,
+  version: _kisskhVersion,
+  itemType: ItemType.anime,
+);
