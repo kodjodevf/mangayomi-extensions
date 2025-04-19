f0d562259c4ee5830d814338fbd6ace8a9751acb
xShader1374 <78726437+xShader1374@users.noreply.github.com>
2025-04-19 12:36:06 +0200

    Update mangaworld.js

--- javascript/manga/src/it/mangaworld.js
+++ javascript/manga/src/it/mangaworld.js
@@ -255,7 +255,8 @@ class DefaultExtension extends MProvider {
                     ['2021', '2021'],
                     ['2022', '2022'],
                     ['2023', '2023'],
-                    ['2024', '2024']
+                    ['2024', '2024'],
+                    ['2025', '2025']
                 ].map(x => ({ type_name: 'CheckBox', name: x[0], value: x[1] }))
             },
             {
