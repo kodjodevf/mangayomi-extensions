70f6654d8c07e0d687bd33a2752d12b958b696a1
Schnitzel5 <tangphilip3@gmail.com>
2025-05-07 22:38:04 +0200

    +

--- javascript/novel/src/en/wordrain69.js
+++ javascript/novel/src/en/wordrain69.js
@@ -7,7 +7,7 @@ const mangayomiSources = [{
     "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/main/javascript/icon/en.wordrain69.png",
   "typeSource": "single",
   "itemType": 2,
-  "version": "0.0.3",
+  "version": "0.0.4",
   "dateFormat": "",
   "dateFormatLocale": "",
   "pkgPath": "novel/src/en/wordrain69.js",
