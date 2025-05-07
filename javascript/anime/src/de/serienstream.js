cb36db735592f87b26c1e1d811f8f6dd4446baca
NBA2K1 <78034913+NBA2K1@users.noreply.github.com>
2025-05-07 18:49:52 +0200

    version++

--- javascript/anime/src/de/serienstream.js
+++ javascript/anime/src/de/serienstream.js
@@ -7,7 +7,7 @@ const mangayomiSources = [{
     "typeSource": "single",
     "itemType": 1,
     "isNsfw": false,
-    "version": "0.0.8",
+    "version": "0.0.9",
     "dateFormat": "",
     "dateFormatLocale": "",
     "pkgPath": "anime/src/de/serienstream.js"
