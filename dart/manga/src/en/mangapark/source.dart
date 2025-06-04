341d53dac05df0d4b85065b699935f9debf26924
KptnFishy <120373460+KptnFishy@users.noreply.github.com>
2025-06-04 20:17:10 +0200

    Add source.dart

---
+++ dart/manga/src/en/mangapark/source.dart
@@ -0,0 +1,19 @@
+import '../../../../../model/source.dart';
+
+Source get mangaparkSource => _mangaparkSource;
+const _mangaparkVersion = "1.0.0";
+const _mangaparkSourceCodeUrl = "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/$branchName/dart/manga/src/en/mangahere/mangahere.dart";
+const _mangaparkIconUrl = "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/$branchName/dart/manga/src/en/mangapark/icon.png";
+Source _mangaparkSource = Source(
+  name: "MangaPark",
+  baseUrl: "http://www.mangapark.io",
+  apiUrl: "http://www.mangapark.io/apo/",
+  lang: "en",
+  typeSource: "single",
+  iconUrl: _mangaparkIconUrl,
+  sourceCodeUrl: _mangaparkSourceCodeUrl,
+  itemType: ItemType.manga,
+  version: _mangaparkVersion,
+  dateFormat: "MMM dd yyyy",
+  dateFormatLocale: "en",
+);
