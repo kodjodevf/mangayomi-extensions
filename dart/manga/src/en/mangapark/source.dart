eb0c6355513c6a024ec5547a8a394de02d9dc15b
KptnFishy <120373460+KptnFishy@users.noreply.github.com>
2025-06-05 00:13:57 +0200

    Update source.dart - URL fix

--- dart/manga/src/en/mangapark/source.dart
+++ dart/manga/src/en/mangapark/source.dart
@@ -6,9 +6,9 @@ const _mangaparkSourceCodeUrl = "https://raw.githubusercontent.com/kodjodevf/man
 const _mangaparkIconUrl = "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/$branchName/dart/manga/src/en/mangapark/icon.png";
 Source _mangaparkSource = Source(
   name: "MangaPark",
-  baseUrl: "http://www.mangapark.io",
-  apiUrl: "http://www.mangapark.io/apo/",
-  lang: "en",
+  baseUrl: "https://mangapark.io",
+  apiUrl: "https://mangapark.io/apo/",
+  lang: "en", 
   typeSource: "single",
   iconUrl: _mangaparkIconUrl,
   sourceCodeUrl: _mangaparkSourceCodeUrl,
