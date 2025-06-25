a3b7a8f2ef4fd7ce3bd6fd21f9f0e7d97d22e26d
xMohnad <mohnad00967@gmail.com>
2025-06-25 21:28:49 +0000

    mangareader: Add Eros Scan (ES) manga source (#177)

---
+++ dart/manga/multisrc/mangareader/src/en/erosscans/erosscans.dart
@@ -0,0 +1,14 @@
+import '../../../../../../../model/source.dart';
+
+Source get erosscansSource => _erosscansSource;
+
+Source _erosscansSource = Source(
+  name: "Eros Scans",
+  baseUrl: "https://eros-void.xyz",
+  lang: "en",
+  typeSource: "mangareader",
+  iconUrl:
+      "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/$branchName/dart/manga/multisrc/mangareader/src/en/erosscans/icon.png",
+  dateFormat: "MMMM dd, yyyy",
+  dateFormatLocale: "en_us",
+);
