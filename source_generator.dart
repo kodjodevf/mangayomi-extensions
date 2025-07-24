d8a592157bb1a878c775adf879912e82e4dd244d
Moustapha Kodjo Amadou <107993382+kodjodevf@users.noreply.github.com>
2025-07-24 09:24:02 +0100

    Remove anime_index.json generation from source generator

--- source_generator.dart
+++ source_generator.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:developer';
 import 'dart:io';
-import 'dart/anime/anime_source_list.dart';
 import 'dart/manga/manga_source_list.dart';
 import 'dart/novel/novel_source_list.dart';
 import 'model/source.dart';
@@ -10,9 +9,6 @@ void main() {
   final jsSources = _searchJsSources(Directory("javascript"));
   genManga(
     jsSources.where((element) => element.itemType!.name == "manga").toList(),
-  );
-  genAnime(
-    jsSources.where((element) => element.itemType!.name == "anime").toList(),
   );
   genNovel(
     jsSources.where((element) => element.itemType!.name == "novel").toList(),
@@ -23,25 +19,12 @@ void genManga(List<Source> jsMangasourceList) {
   List<Source> mangaSources = [];
   mangaSources.addAll(dartMangasourceList);
   mangaSources.addAll(jsMangasourceList);
-  final List<Map<String, dynamic>> jsonList =
-      mangaSources.map((source) => source.toJson()).toList();
+  final List<Map<String, dynamic>> jsonList = mangaSources
+      .map((source) => source.toJson())
+      .toList();
   final jsonString = jsonEncode(jsonList);
 
   final file = File('index.json');
-  file.writeAsStringSync(jsonString);
-
-  log('JSON file created: ${file.path}');
-}
-
-void genAnime(List<Source> jsAnimesourceList) {
-  List<Source> animeSources = [];
-  animeSources.addAll(dartAnimesourceList);
-  animeSources.addAll(jsAnimesourceList);
-  final List<Map<String, dynamic>> jsonList =
-      animeSources.map((source) => source.toJson()).toList();
-  final jsonString = jsonEncode(jsonList);
-
-  final file = File('anime_index.json');
   file.writeAsStringSync(jsonString);
 
   log('JSON file created: ${file.path}');
@@ -51,8 +34,9 @@ void genNovel(List<Source> jsNovelSourceList) {
   List<Source> novelSources = [];
   novelSources.addAll(dartNovelSourceList);
   novelSources.addAll(jsNovelSourceList);
-  final List<Map<String, dynamic>> jsonList =
-      novelSources.map((source) => source.toJson()).toList();
+  final List<Map<String, dynamic>> jsonList = novelSources
+      .map((source) => source.toJson())
+      .toList();
   final jsonString = jsonEncode(jsonList);
 
   final file = File('novel_index.json');
@@ -80,13 +64,12 @@ List<Source> _searchJsSources(Directory dir) {
           if (match != null) {
             for (var sourceJson in jsonDecode(match.group(1)!) as List) {
               final langs = sourceJson["langs"] as List?;
-              Source source =
-                  Source.fromJson(sourceJson)
-                    ..sourceCodeLanguage = 1
-                    ..appMinVerReq =
-                        sourceJson["appMinVerReq"] ?? defaultSource.appMinVerReq
-                    ..sourceCodeUrl =
-                        "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/$branchName/javascript/${sourceJson["pkgPath"] ?? sourceJson["pkgName"]}";
+              Source source = Source.fromJson(sourceJson)
+                ..sourceCodeLanguage = 1
+                ..appMinVerReq =
+                    sourceJson["appMinVerReq"] ?? defaultSource.appMinVerReq
+                ..sourceCodeUrl =
+                    "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/$branchName/javascript/${sourceJson["pkgPath"] ?? sourceJson["pkgName"]}";
               if (sourceJson["id"] != null) {
                 source = source..id = int.tryParse("${sourceJson["id"]}");
               }
